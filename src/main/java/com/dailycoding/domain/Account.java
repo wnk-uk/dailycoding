32f32b8b5f5b461ab23d24d7232ca0ba68dc249d
남궁욱 <namgung-ug@namgung-ug-ui-MacBookPro.local>
2020-11-11 00:26:37 +0900

    Initial commit

---
+++ src/main/java/com/dailycoding/domain/Account.java
@@ -0,0 +1,52 @@
+package com.dailycoding.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @NoArgsConstructor @AllArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location; //varchar(255)
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreateByEmail;
+
+    private boolean studyCreateByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdateByEmail;
+
+    private boolean studyUpdateByWeb;
+}
