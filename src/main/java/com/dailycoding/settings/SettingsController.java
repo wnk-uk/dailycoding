c73a1cf08ba4ba9bb96a7b0597cd7298807632f6
wnk-uk <74255991+wnk-uk@users.noreply.github.com>
2021-12-06 20:30:40 +0900

    desc

--- src/main/java/com/dailycoding/settings/SettingsController.java
+++ src/main/java/com/dailycoding/settings/SettingsController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -22,21 +23,23 @@ public class SettingsController {
 
     private final AccountService accountService;
 
-    @GetMapping("/settings/profile")
+    @GetMapping(SETTINGS_PROFILE_URL)
     public String profileUpdateForm(@CurrentUser Account account, Model model) {
         model.addAttribute(account);
         model.addAttribute(new Profile(account));
         return SETTINGS_PROFILE_VIEW_NAME;
     }
 
-    @PostMapping("settings/profile")
-    public String updateProfile(@CurrentUser Account account, @Valid Profile profile, Errors errors, Model model) {
+    @PostMapping("/settings/profile")
+    public String updateProfile(@CurrentUser Account account, @Valid Profile profile, Errors errors,
+                                Model model, RedirectAttributes attributes) {
         if(errors.hasErrors()) {
             model.addAttribute(account);
             return SETTINGS_PROFILE_VIEW_NAME;
         }
 
         accountService.updateProfile(account, profile);
+        attributes.addFlashAttribute("message", "프로필을 수정했습니다.");
         return "redirect:" + SETTINGS_PROFILE_URL;
     }
 }
